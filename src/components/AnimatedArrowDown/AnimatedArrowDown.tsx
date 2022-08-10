6e3a70a04050a787d16f56c2b97c1d0d000d8aa5
Vítor Paes <vitor.paes@compasso.com.br>
2022-08-10 04:04:45 -0300

    Add animated arrow down on homepage

---
+++ src/components/AnimatedArrowDown/AnimatedArrowDown.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { motion } from 'framer-motion'
+import { FiArrowDown } from 'react-icons/fi'
+import { styled } from '@mui/material'
+import { isMobile } from '@app/drivers/device'
+
+interface StyledArrowDownWapperProps {
+    isMobile: boolean
+}
+
+const StyledArrowDownWapper = styled(motion.div)<StyledArrowDownWapperProps>`
+    position: absolute;
+    bottom: ${({ theme, isMobile }) =>
+        isMobile ? theme.size.large : theme.size.huge}px;
+`
+
+const AnimatedArrowDown: React.FC = () => {
+    const isDeviceMobile = isMobile()
+
+    const moveTop = -50
+
+    return (
+        <StyledArrowDownWapper
+            isMobile={isDeviceMobile}
+            animate={{
+                y: moveTop,
+            }}
+            transition={{
+                repeat: Infinity,
+                duration: 0.25,
+            }}
+        >
+            <FiArrowDown size={35} />
+        </StyledArrowDownWapper>
+    )
+}
+
+export default AnimatedArrowDown
